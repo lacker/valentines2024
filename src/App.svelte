12c7eb40fd79178bc3904129aaeb8fa9a3dd7414
Kevin Lacker <lacker@gmail.com>
2024-02-12 11:14:41 -0800

    handling keypresses

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,60 @@
 <script>
+  import { onMount, onDestroy } from "svelte";
+
   let grid = [
     ["A", "B", "C", "D"],
     ["E", "F", "G", "H"],
     ["I", "J", "K", "L"],
     ["M", "N", "O", "P"],
   ];
+
+  function shuffle_rows() {
+    grid = grid.map((row) => row.sort(() => Math.random() - 0.5));
+  }
+
+  function handleUp() {
+    console.log("up");
+  }
+
+  function handleDown() {
+    console.log("down");
+  }
+
+  function handleLeft() {
+    console.log("left");
+  }
+
+  function handleRight() {
+    console.log("right");
+  }
+
+  function handleKeyPress(event) {
+    switch (event.key) {
+      case "ArrowUp":
+        handleUp();
+        break;
+      case "ArrowDown":
+        handleDown();
+        break;
+      case "ArrowLeft":
+        handleLeft();
+        break;
+      case "ArrowRight":
+        handleRight();
+        break;
+      default:
+        // Not an arrow key
+        break;
+    }
+  }
+
+  onMount(() => {
+    window.addEventListener("keydown", handleKeyPress);
+  });
+
+  onDestroy(() => {
+    window.removeEventListener("keydown", handleKeyPress);
+  });
 </script>
 
 <div class="flex justify-center items-center h-screen">
