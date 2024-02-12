1162bdd14d92a5f6c277fbb0119b9cb44fea8d84
Kevin Lacker <lacker@gmail.com>
2024-02-12 10:55:05 -0800

    fix grid squareness

--- src/App.svelte
+++ src/App.svelte
@@ -7,14 +7,16 @@
   ];
 </script>
 
-<div class="grid grid-cols-4 gap-2">
-  {#each grid as row}
-    {#each row as character}
-      <div
-        class="aspect-w-1 aspect-h-1 w-32 h-32 flex justify-center items-center border border-black"
-      >
-        {character}
-      </div>
+<div class="flex justify-center items-center h-screen">
+  <div class="grid grid-cols-4 gap-2">
+    {#each grid as row}
+      {#each row as character}
+        <div
+          class="aspect-w-1 aspect-h-1 w-32 h-32 flex justify-center items-center border border-black"
+        >
+          {character}
+        </div>
+      {/each}
     {/each}
-  {/each}
+  </div>
 </div>
