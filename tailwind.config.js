2dd2d1c33a229c1e54344c066dfa28707ff7216b
Kevin Lacker <lacker@gmail.com>
2024-02-12 10:52:31 -0800

    grid works

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.svelte', './public/index.html'],
+  content: ['./src/**/*.{js,svelte,html}', './public/index.html'],
   theme: {
     extend: {},
   },
